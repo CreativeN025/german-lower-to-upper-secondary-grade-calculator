56d3e7f0e1c5cda056124ab135543ef9e04497b9
creative name <creativename0505@gmail.com>
2023-11-30 17:34:22 +0100

    update

--- Program.cs
+++ Program.cs
@@ -8,7 +8,6 @@
             Console.WriteLine("how many subjects do you want to calculate");
             int subjects = Int16.Parse(Console.ReadLine());
             int[] grades = new int[subjects];
-            //int[] grades = { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1 };
 
             for (int i = 0; i < grades.Length; i++)
             {
@@ -24,6 +23,7 @@
             Console.WriteLine("how many -'s do you have (example 2-)");
             int minuscount = Int16.Parse(Console.ReadLine());
             pluscount = pluscount - minuscount;
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine($"sum: {grades.Sum()} subjects: {subjects} average: {Math.Round(average, 2)}");
             Console.WriteLine("points: " + calculatepoints(grades, pluscount, false));
             Console.WriteLine($"GPA: {calculatepoints(grades, pluscount, true)}");
@@ -33,7 +33,6 @@
         {
             double points = 0;
             double[] pointlist = { 14, 11, 8, 5, 2, 0 };
-            double[] GPAlist = { 0, 0, 0, 0, 1.0, 1.3, 1.7, 2.0, 2.3, 2.7, 3.0, 3.3, 3.7, 4.0, 4.0 };
             double ResultGPA = 0;
             for (int i = 0; i < grades.Length; i++)
             {
